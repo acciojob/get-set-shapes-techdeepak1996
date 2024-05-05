ed37490cd5571079a9d2ebace8b7859be1c73b2b
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-06 01:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//complete this code
 class Rectangle {
 	constructor(width, height) {
     this._width = width;
@@ -18,7 +17,7 @@ class Rectangle {
   }
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	constructor(side) {
     super(side, side);
     this._side = side;
